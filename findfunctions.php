b2a152fea6e8f2ada3e50b8e1e723ef716a37527
Ozgur Sulum <ozgursulum0@gmail.com>
2021-03-19 00:42:42 +0300

    ister2 form oluşturuldu, işlemler fonksiyonlara ayrıldı, diziden gereksiz olan frekanslar çıkartıldı.

---
+++ findfunctions.php
@@ -0,0 +1,54 @@
+<?php
+  //html_dom_parser kütüphanesini ekledik.
+  //LİNK = "https://simplehtmldom.sourceforge.io/"
+include('simple_html_dom.php');
+function findFreq($url){
+    //html dosyasını aldık.
+    $html = file_get_html($url);
+    
+    //tagsiz halini aldık
+    $htmlPlainText = $html->plaintext;
+
+    //filtrele
+    $htmlPlainText = preg_replace('/[^\p{L}\p{N}\s]/u', ' ', $htmlPlainText);
+    $htmlPlainText = preg_replace('/[0-9]+/', '', $htmlPlainText);
+    $htmlPlainText = str_ireplace(array('nbsp','gt','lt','div','br'),' ',$htmlPlainText);
+    $htmlPlainText = strtolower($htmlPlainText);
+
+    //kelimelerin frekansları hesaplanır.
+    $wordArray = str_word_count($htmlPlainText,1);
+    $wordFreqArray = array_count_values($wordArray);
+    arsort($wordFreqArray);
+    
+    return $wordFreqArray;
+}
+  
+  
+
+  function printFreq($wordFreqArray){
+    echo "<h1> Keywords </h1>";
+    //bulunan kelimelerin frekanslarını ekrana yazdır. (uzunluğu 3'den küçükse yazdırma)
+    if($wordFreqArray != null){
+     echo $_GLOBAL["URL"];
+     echo "<br><br>
+     <table>
+     <tr>
+       <th>Kelime</th>
+       <th>Frekans</th> 
+     </tr>";
+   }
+   
+   foreach($wordFreqArray as $key => $value){
+     if(strlen($key) >= 3){
+       echo "<tr>
+             <td>$key</td>
+             <td>$value</td>
+           </tr>";
+     }
+     
+   }
+   echo "</table>";
+  }
+      
+
+?>
--- ister_2.php
+++ ister_2.php
@@ -1,13 +1,66 @@
 <?php
+  include('findfunctions.php');
+  if($_SERVER["REQUEST_METHOD"] == "POST"){  
+    $_GLOBAL["freqArray1"] = findFreq($_POST["URL1"]);
+    $_GLOBAL["freqArray2"] = findFreq($_POST["URL2"]);
+    
+  }
+  
+  function findKeyword($wordFreqArray){
+    $stopwords = array("is","not","that","there","are","can","you","with","of","those","after","all","one");
+    $keywords;
+    //stopwordslerin bulunmadığı yeni bir array oluşturuyoruz.
+    
+    foreach($wordFreqArray as $key => $value){
+      $inside = 1;
+      for($i = 0; $i<count($stopwords); $i++){
+        
+        if($key == $stopwords[$i]){
+          $inside = 0;
+        }
+       
+      }
+      if($inside){
+        $keywords[$key] = $value;;
+      }
+    
+    }
+    
+    //BURADA FREKANSA GORE VS KEYWORDLERİ BULUP DONDUREBİLİRİZ. şimdilik yukardaki kadar.
 
-
+    return $keywords;
+  }
+  
 ?>
 
 <!DOCTYPE html>
 <html>
   <?php include('templates/header.php'); ?>
   <?php include('templates/navbar.php'); ?>
-  
+
+  <div class="form_style">
+    <h4> Sayfadaki keywordleri ve sitelerin benzerliklerini bulma.<br> <br></h4>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
+      <label for="URL">Birinci URL:</label><br>
+      <textarea name="URL1" rows="1" cols="50"></textarea><br>
+      <label for="URL">İkinci URL:</label><br>
+      <textarea name="URL2" rows="1" cols="50"></textarea><br>
+      <input type="submit" style="margin-top: 20px;">
+    </form>
+
+    <?php
+    $keywords1 = findKeyword($_GLOBAL["freqArray1"]);
+    $keywords2 = findKeyword($_GLOBAL["freqArray2"]);
+    if(($keywords1 != null) && ($keywords2 != null)){
+      printFreq($keywords1);
+      printFreq($keywords2);
+    }
+    
+    ?>
+
+  </div>
+
+
   <?php include('templates/footer.php'); ?>
 
 </html>
