91e7f01debd4ade1a2a1052f069b7fb456efd3f4
Ozgur Sulum <ozgursulum0@gmail.com>
2021-03-20 00:16:06 +0300

    ister2 ve ister3 isterlerde verilen gibi yapıldı.

--- ister_2.php
+++ ister_2.php
@@ -2,13 +2,15 @@
   include('findfunctions.php');
   $keywords1;
   $keywords2;
+  $similarityScore;
   if($_SERVER["REQUEST_METHOD"] == "POST"){  
     $_GLOBAL["freqArray1"] = findFreq($_POST["URL1"]);
     $_GLOBAL["freqArray2"] = findFreq($_POST["URL2"]);
     $keywords1 = findKeyword($_GLOBAL["freqArray1"]);
     $keywords2 = findKeyword($_GLOBAL["freqArray2"]);
+    $similarityScore = findComparison($keywords1, $keywords2, $_GLOBAL["freqArray2"]);
   }
-  
+  //keyword bulma fonksiyonu geriye keyword dizisini döndürür.
   function findKeyword($wordFreqArray){
     //stopwordsleri stopwords.txt dosyasından alarak dizi oluşturuyoruz.
     $stopwords = array();
@@ -40,7 +42,7 @@
     //ŞİMDİLİK EN YÜKSEK FREKANSA SAHİP 5 KELİMEYİ KEYWORD OLARAK SEÇİYORUZ
     arsort($filteredWordFreqArray);
     $keywords = array();
-    $numberOfKeywords = 5;
+    $numberOfKeywords = 10;
     foreach($filteredWordFreqArray as $keyword => $frequency){
       if($numberOfKeywords > 0){
         $keywords[$keyword] = $frequency;
@@ -55,6 +57,33 @@
     return $keywords;
   }
   
+  //iki site arasındaki benzerliği bulmamıza yarayan fonksiyon
+  function findComparison($wordFreqArray1, $wordFreqArray2, $wordFreqArray3){
+    $tmpArray = array();
+    
+    foreach($wordFreqArray1 as $key1 => $value1){
+      foreach($wordFreqArray2 as $key2 => $value2){
+        if($key1 == $key2){
+          $tmpArray[$key1] = $value2;
+          break;
+        }
+      }
+    }
+
+    $sumFreq = 0;
+    foreach($wordFreqArray3 as $key => $value){
+      $sumFreq += $value;
+    }
+
+    $multiply = 1;
+    foreach($tmpArray as $key => $value){
+      $multiply *= $value;
+      echo "$multiply <br>";
+    }
+    
+    echo $sumFreq;
+    return $multiply / $sumFreq;
+  }
 ?>
 
 <!DOCTYPE html>
@@ -73,12 +102,13 @@
     </form>
 
     <?php
-    
-    if(($keywords1 != null) && ($keywords2 != null)){
+    //&& ($similarityScore != null)
+    if(($keywords1 != null) && ($keywords2 != null) ){
       echo "<h1> Keywords </h1>";
       printFreq($keywords1);
       echo "<h1> Keywords </h1>";
       printFreq($keywords2);
+      echo "<br> <h6> Benzerlik:</h6> $similarityScore";
     }
     
     ?>
