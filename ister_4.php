de8a6714527e0ef1e79ec9202cccd61915d01f19
Ozgur Sulum <ozgursulum0@gmail.com>
2021-03-20 21:54:55 +0300

    ister-4 url girişi oluşturuldu.

--- ister_4.php
+++ ister_4.php
@@ -1,6 +1,16 @@
 <?php
   include("findFunctions.php");
-
+  if($_SERVER["REQUEST_METHOD"] == "POST"){  
+    $URLS = $_POST["URLS"];
+    $URL_array = explode("\n",$URLS);
+    $number = 1;
+    foreach($URL_array as $URL){
+      echo "$number-) $URL <br>";
+      $number++;
+    }
+   
+  }
+  
 ?>
 
 <!DOCTYPE html>
@@ -9,10 +19,11 @@
   <?php include('templates/navbar.php'); ?>
 
   <div class="form_style">
-    <h4> Site indexleme ve siralama.<br> <br></h4>
+    <h4> Site indexleme ve siralama.<br></h4>
+    <h6> Her url girişin sonunda enter tuşuna basarak bir alt satır geçilmelidir.</h6>
     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
-      <label for="URL">URL:</label><br>
-      <textarea name="URL" rows="1" cols="50"></textarea><br>
+      <label for="URLS"><h6> URLS:</h6></label><br>
+      <textarea name="URLS" rows="20" cols="200"></textarea><br>
       <input type="submit" style="margin-top: 20px;">
     </form>
   </div>
