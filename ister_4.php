de1d3169e578416c120405d712a520ec6a6e28a5
Ozgur Sulum <ozgursulum0@gmail.com>
2021-03-23 03:13:59 +0300

    urlTree classi olusturuldu

--- ister_4.php
+++ ister_4.php
@@ -3,63 +3,142 @@
   if($_SERVER["REQUEST_METHOD"] == "POST"){  
     $URLS = $_POST["URLS"];
     $URL = $_POST["URL"];
-    comparisonAll($URLS, $URL);
+    $url_array = explode("\n",$URLS);
+    comparisonAll($url_array, $URL);
     // newline'a göre url'leri ayırdık.
-    $_GLOBAL["url_array"] = explode("\n",$URLS);
-    findChildUrls($_GLOBAL["url_array"]);
+    //findChildUrls($url_array);
     
   }
+  class UrlTree{
+    public $node;
+    public $already_crawled;
+    function __construct($parentURL, $currentDepth){
+      $this->already_crawled = array();
+      $this->node = $this->recursive($parentURL,$currentDepth);
+      
+    }
+    function recursive($parentURL, $currentDepth){
+      $node_array = array();
+      $node_array["parent"] = $parentURL;
+      $node_array["child"] = array();
+      if($currentDepth < 2){
+        $child_url = array();
+        $child_urls = $this->findChildUrl($node_array["parent"],$currentDepth);
+        foreach($child_urls as $child_url){
+          $node_array["child"][] = $this->recursive($child_url, $currentDepth+1);
+        }
+      }
+      return $node_array;
+    }
+
+    function findChildUrl($url, $currentDepth){
+      
+      $crawling = array();
+      $html = file_get_html(trim($url));
+      $links = $html->find('a');
+      
+      foreach($links as $link){
+        $l =  $link->href;
+        
+        $l = $this->linkFilter($l, $url);
+        if($l == null){
+          continue;
+        }
+        
+        // If the link isn't already in our crawl array add it, otherwise ignore it.
+        if (!in_array($l, $this->already_crawled)) {
+            $this->already_crawled[] = $l;
+            $crawling[] = $l;
+            
+        }
+      }
+      return $crawling; 
+    }
+
+    function linkFilter($l, $url){
+      // Process all of the links we find. This is covered in part 2 and part 3 of the video series.
+      if (substr($l, 0, 1) == "/" && substr($l, 0, 2) != "//") {
+        return parse_url($url)["scheme"]."://".parse_url($url)["host"].$l;
+      } else if (substr($l, 0, 2) == "//") {
+        return parse_url($url)["scheme"].":".$l;
+      } else if (substr($l, 0, 2) == "./") {
+        return parse_url($url)["scheme"]."://".parse_url($url)["host"].dirname(parse_url($url)["path"]).substr($l, 1);
+      } else if (substr($l, 0, 1) == "#") {
+        return parse_url($url)["scheme"]."://".parse_url($url)["host"].parse_url($url)["path"].$l;
+      } else if (substr($l, 0, 3) == "../") {
+        return parse_url($url)["scheme"]."://".parse_url($url)["host"]."/".$l;
+      } else if (substr($l, 0, 11) == "javascript:") {
+        return null;
+      } else if (substr($l, 0, 5) != "https" && substr($l, 0, 4) != "http") {
+        return parse_url($url)["scheme"]."://".parse_url($url)["host"]."/".$l;
+      }
+    }
+
+    
+    static function printTree($node, $currentDepth){
+      $parent = $node["parent"];
+      $child_array = $node["child"];
+      
+      
+      echo str_repeat("&nbsp&nbsp",$currentDepth);
+      if(empty($child_array)){
+        echo "- ";
+      }else{
+        echo "+ ";
+      }
+
+      echo "$parent <br>";
+
+      foreach($child_array as $child){
+        self::printTree($child, $currentDepth+1);
+      }
+      
+    }
+    
+  }
+ 
 
   function comparisonAll($URLS, $URL){
     //OLAY BURADA GEÇECEK DE NASIL GEÇECEK ALTTAKİ FONKSİYONLAR DA DEĞİŞİR BÜYÜK İHTİMALLE
     //ALTTAKİ FONKSİYON ŞİMDİLİK LİNKLERİ GÖSTERİYOR FİLTRELEYİP
-  }
+    
+    //FREKANSLAR BULUNUR
+    $wordFreqArray0 = findFreq($URL);
+    //matris yapisi seklinde olacak. 0.index => 1.url'nin frekans dizisi ... şeklinde
+    $wordFreqArray1 = array();
+    foreach($URLS as $url){
+      $wordFreqArray1[$url] = findFreq(trim($url));
+    }
+    
+    //KEYWORDLER BULUNUR
+    $keywordArray0 = findKeyword($wordFreqArray0);
+    //matris yapisi şeklinde olacak. 0.index => 1.url'nin keywords dizisi ... şeklinde
+    $keywordArray1 = array();
+    foreach($wordFreqArray1 as $url => $wordFreqArray){
+      $keywordArray1[$url] = findKeyword($wordFreqArray);
+    }
 
-  function findChildUrls($urls){
-    foreach($urls as $url){
-      $already_crawled = array();
-      $crawling = array();
-      $html = file_get_html(trim($url));
-      $links = $html->find('a');
-     
-      foreach($links as $link){
-        $l =  $link->href;
-        
-        $l = linkFilter($l, $url);
-        if($l == null){
-          continue;
-        }
-
-        // If the link isn't already in our crawl array add it, otherwise ignore it.
-        if (!in_array($l, $already_crawled)) {
-            $already_crawled[] = $l;
-            $crawling[] = $l;
-            echo "$l <br>";
-            
-        }
+    //URL ağacı çıkarılır.
+    $urlTree = new UrlTree($URL,0);
+    UrlTree::printTree($urlTree->node,0);
+    
+    /*
+    $urlTrees = array();
+    foreach($URLS as $url){
+      $urlTrees[$url] = new UrlTree($url,0); 
+    }
+    
+    foreach($URLS as $url){
+      if($urlTrees[$url] != null){
+        UrlTree::printTree($urlTrees[$url],0);
+        echo "<br> <br> <br>";
       }
     }
-  }
-  
-  function linkFilter($l, $url){
-    // Process all of the links we find. This is covered in part 2 and part 3 of the video series.
-    if (substr($l, 0, 1) == "/" && substr($l, 0, 2) != "//") {
-      return parse_url($url)["scheme"]."://".parse_url($url)["host"].$l;
-    } else if (substr($l, 0, 2) == "//") {
-      return parse_url($url)["scheme"].":".$l;
-    } else if (substr($l, 0, 2) == "./") {
-      return parse_url($url)["scheme"]."://".parse_url($url)["host"].dirname(parse_url($url)["path"]).substr($l, 1);
-    } else if (substr($l, 0, 1) == "#") {
-      return parse_url($url)["scheme"]."://".parse_url($url)["host"].parse_url($url)["path"].$l;
-    } else if (substr($l, 0, 3) == "../") {
-      return parse_url($url)["scheme"]."://".parse_url($url)["host"]."/".$l;
-    } else if (substr($l, 0, 11) == "javascript:") {
-      return null;
-    } else if (substr($l, 0, 5) != "https" && substr($l, 0, 4) != "http") {
-      return parse_url($url)["scheme"]."://".parse_url($url)["host"]."/".$l;
-    }
+    */
+
   }
 
+  
 ?>
 
 <!DOCTYPE html>
---
+++ test.php
@@ -0,0 +1,22 @@
+<?php 
+
+
+
+
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+
+
+</head>
+<body>
+
+<a href="test.php">link1</a>
+<a href="test1.php">link2</a>
+<a href="test3.php">link3</a>
+<body>
+
+</html>
